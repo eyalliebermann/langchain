b0499073ef3d9ab48f501ef88db80c9e5e9a9ea9
Eyal M3 Liebermann <m3@eyal.io>
2024-07-19 01:17:33 +0200

    install and configure nbstripout to filter out notebook output

--- do/execute.notebooks.zsh
+++ do/execute.notebooks.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 set -e  # Exit immediately if a command exits with a non-zero status.
-set -x  # Print commands and their arguments as they are executed.
+#set -x  # Print commands and their arguments as they are executed.
 echo $SHELL
 echo "Executing script: $0 with arguments: $@"
 
@@ -15,8 +15,10 @@ echo "Project root: $PROJECT_ROOT"
 ENV_FILE="$PROJECT_ROOT/.env"
 VENV_DIR="$PROJECT_ROOT/venv.notebooks"
 REQUIREMENTS_FILE="$PROJECT_ROOT/requirements.notebooks.txt"
+GITATTRIBUTES_FILE="$PROJECT_ROOT/.gitattributes"
 
-# Change directory to the top directory (assuming it's one level up from the script's location)
+
+# Change directory to the top directory
 cd "$PROJECT_ROOT"
 echo "Changed directory to project root: $(pwd)"
 
@@ -49,16 +51,49 @@ fi
 # Register the virtual environment as a Jupyter Notebook kernel
 "$VENV_DIR/bin/python" -m ipykernel install --user --name="langchain.notebooks" --display-name="LangChain Notebooks"
 
+# Add nbstripout to requirements if not already present
+if ! grep -q "nbstripout" "$REQUIREMENTS_FILE"; then
+    echo "nbstripout" >> "$REQUIREMENTS_FILE"
+    echo "Added nbstripout to $REQUIREMENTS_FILE"
+fi
+
 # Upgrade pip and install requirements
 echo "\nInstalling required dependencies..."
 "$VENV_DIR/bin/pip" install --upgrade pip
 "$VENV_DIR/bin/pip" install --upgrade -r "$REQUIREMENTS_FILE"
 
+# Set up nbstripout in git
+"$VENV_DIR/bin/nbstripout" --install
+
+
+# Update .gitattributes
+if [[ ! -f "$GITATTRIBUTES_FILE" ]]; then
+    echo "*.ipynb filter=nbstripout" > "$GITATTRIBUTES_FILE"
+    echo "*.ipynb diff=ipynb" >> "$GITATTRIBUTES_FILE"
+    echo "Created $GITATTRIBUTES_FILE with nbstripout configuration"
+else
+    if ! grep -q "filter=nbstripout" "$GITATTRIBUTES_FILE"; then
+        echo "*.ipynb filter=nbstripout" >> "$GITATTRIBUTES_FILE"
+        echo "Added nbstripout filter to $GITATTRIBUTES_FILE"
+    fi
+    if ! grep -q "diff=ipynb" "$GITATTRIBUTES_FILE"; then
+        echo "*.ipynb diff=ipynb" >> "$GITATTRIBUTES_FILE"
+        echo "Added ipynb diff to $GITATTRIBUTES_FILE"
+    fi
+fi
+
+# Verify nbstripout installation
+if "$VENV_DIR/bin/nbstripout" --is-installed; then
+    echo "nbstripout has been successfully set up for this project."
+else
+    echo "Error: nbstripout installation failed."
+    exit 1
+fi
+
+
 # Change to the notebooks directory
-cd "$NOTEBOOKS_DIR"
+cd "$PROJECT_ROOT/notebooks"
 
 # Run Jupyter Notebook
 echo "\nRunning Jupyter Notebook ..."
 "$VENV_DIR/bin/jupyter" notebook
-
-
